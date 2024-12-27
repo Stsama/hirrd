7b1f5fa191827026db7d4affec00e1f1281d29c9
st sama <albertezoula98@gmail.com>
2024-12-27 02:21:19 +0000

    modification of the carrousel z-index

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -44,7 +44,7 @@ const LandingPage = () => {
             delay: 2000,
           }),
         ]}
-        className="w-full py-10"
+        className="w-full py-10 z-40"
       >
         <CarouselContent className="flex gap-5 sm:gap-20 items-center">
           {companies.map(({ name, id, path }) => (
